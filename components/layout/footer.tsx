cc08180e5abacbf47bb078630d3b41944bf6d671
eldrige <peldriige8@gmail.com>
2023-07-06 23:05:55 +0100

    style: design footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,10 +1,59 @@
+import Image from "next/image"
 import React from "react"
 import Container from "./containter"
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
   return (
-    <section className="w-full bg-[#f9f9f9] px-4 py-16">
-      <Container classNames="bg-[#f9f9f9] flex items-center justify-between">Footer</Container>
+    <section className="mt-60 w-full bg-[#f9f9f9]">
+      <Container classNames="bg-[#f9f9f9] text-black/30 flex items-start justify-between py-16 px-4">
+        <div className="flex flex-col gap-4">
+          <h1>
+            Falcon <span className="text-blue-800">Innovations</span>
+          </h1>
+          <p className="max-w-[200px]">
+            OurStudio is a digital agency UI / UX Design and Website Development located in Ohio, United States of
+            America
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-blue-800">Service</h1>
+          <p>App development</p>
+          <p>Product design</p>
+          <p>Web development</p>
+          <p>Graphic design</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-blue-800">Company</h1>
+          <p>Our team</p>
+          <p>Portfolio</p>
+          <p>Blog</p>
+          <p>Contact us</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-blue-800">Join our newsletter</h1>
+        </div>
+      </Container>
+      <Container classNames="py-12 px-4 text-black/30 w-full">
+        <div className="mb-8 h-[1px] w-full bg-blue-100" />
+        <div className="flex w-full items-center justify-between">
+          <div>&copy; falconinnovations {currentYear}</div>
+          <div className="flex items-center gap-8">
+            <div className="flex items-center gap-2">
+              <Image src="location_on.svg" alt="arrow_right" width={23} height={23} />
+              Douala, Cameroon
+            </div>
+            <div className="flex items-center gap-2">
+              <Image src="email.svg" alt="arrow_right" width={23} height={23} />
+              contact@falconinnovations.com
+            </div>
+            <div className="flex items-center gap-2">
+              <Image src="call.svg" alt="arrow_right" width={23} height={23} />
+              +237 673993113
+            </div>
+          </div>
+        </div>
+      </Container>
     </section>
   )
 }
